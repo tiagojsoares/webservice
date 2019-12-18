3828734d9c6e9bf9813b700e9435b8eb9451d0aa
Tiago Soares <tiago.soares@controllerbms.com.br>
2019-12-18 16:02:46 -0300

    Query BD

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -13,7 +14,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-    //
+        //
     }
 
     /**
@@ -47,7 +48,65 @@ class HomeController extends Controller
     {
         //
     }
+    public function ObterFuncionarios($token)
+    {
 
+        return DB::table('ACVSCore.Access.Personnel as p')
+        ->select(
+            'p.ObjectID'
+            ,'p.Name'
+            ,'p.Description'
+            ,'p.LastModifiedTime'
+            ,'p.LastName'
+            ,'p.FirstName'
+            ,'p.MiddleName'
+            ,'p.PersonnelTypeID'
+            ,'p.Disabled'
+            ,'p.Text1'
+            ,'p.Text2'
+            ,'p.Text3'
+            ,'p.Text4'
+            ,'p.Text5'
+            ,'p.Text6'
+            ,'p.Text7'
+            ,'p.Text8'
+            ,'p.Text9'
+            ,'p.Text10'
+            ,'p.Text11'
+            ,'p.Text12'
+            ,'p.Text13'
+            ,'p.Text14'
+            ,'p.Text15'
+            ,'p.Text16'
+            ,'p.Text17'
+            ,'p.Text18'
+            ,'p.Text19'
+            ,'p.Text20'
+            ,'p.Text21'
+            ,'p.Text22'
+            ,'p.Text23'
+            ,'p.Text24'
+            ,'p.Text25'
+            ,'p.Int1'
+            ,'p.Int2'
+            ,'p.Int3'
+            ,'p.Int4'
+            ,'p.Int5'
+            ,'p.Int6'
+            ,'p.Int7'
+            ,'p.Int8'
+            ,'p.Int9'
+            ,'p.Logical1'
+            ,'p.Logical2'
+            ,'p.Logical3'
+            ,'p.Logical4'
+            ,'p.Date1'
+            ,'p.Date2'
+            ,'p.Date3'
+            ,'p.Date4'
+            )->join('Access.Credential as c','c.PersonnelId','=','p.ObjectID')
+        ->get();
+    }
     /**
      * Show the form for editing the specified resource.
      *
