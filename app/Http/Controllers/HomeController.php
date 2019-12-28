b637eda5280ca4c0ee96b5ac3e10eeb712d94fba
Tiago Soares <tiago.soares@controllerbms.com.br>
2019-12-28 11:38:47 -0300

    return

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
     public function index()
     {
 
-        $key = md5('P@ssw0rd123');
+        return $key = md5('P@ssw0rd123');
         $timestamp = date('Y-m-d h:i:s');
         $date = date("Y/m/d H:i:s", strtotime("+30 seconds"));
 
